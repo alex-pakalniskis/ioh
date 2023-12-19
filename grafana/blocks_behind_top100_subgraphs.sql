6ed8a52016b70151969f94b4b10f211b146a669c
Alex Pakalniskis <43630382+alex-pakalniskis@users.noreply.github.com>
2023-12-19 07:56:12 -0800

    Create blocks_behind_top100_subgraphs.sql

---
+++ grafana/blocks_behind_top100_subgraphs.sql
@@ -0,0 +1,8 @@
+select
+  s.name,
+  v.deployment,
+    split_part(s.name, '/', 1) as account,
+    split_part(s.name, '/', 2) as subgraph
+from subgraphs.subgraph as s,
+     subgraphs.subgraph_version as v
+where s.current_version = v.id;
